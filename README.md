b9ec0045b89d687f158d9e243ded5bf6bc206dd2
Jithin Raj <jithin6904@gmail.com>
2023-09-13 23:55:53 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# healthyque_app
+
+A new Flutter project.
---
+++ lib/screens/onboarding/onboarding_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class OnbaordingScreen extends StatefulWidget {
+  const OnbaordingScreen({super.key});
+
+  @override
+  State<OnbaordingScreen> createState() => _OnbaordingScreenState();
+}
+
+class _OnbaordingScreenState extends State<OnbaordingScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
